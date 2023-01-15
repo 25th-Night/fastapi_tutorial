e1abba5ad62f927f7c104369e016d14bebc7455f
25th-Night <brown_eyed87@naver.com>
2023-01-15 23:46:11 +0900

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# 추가: 현재 유저를 반환하는 앤드포인트
+@app.get("/users/me")
+def get_current_user():
+    return {"user_id": 123}
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
